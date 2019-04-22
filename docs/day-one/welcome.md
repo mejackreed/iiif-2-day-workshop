d93c036a6bc2a5cd259ee4b262876cbef883ca85
Jack Reed <phillipjreed@gmail.com>
2019-04-22 10:35:31 -0600

    Initial commit

---
+++ docs/day-one/welcome.md
@@ -0,0 +1,16 @@
+---
+title: Welcome
+---
+
+## Introductions
+
+To have a successful workshop, let's first get to know one another.
+* Who are you?
+* Where are you coming from?
+* What's your prior experience with IIIF?
+* What are you hoping to get out of this workshop?
+
+## Overview of workshop
+
+  - Day one
+  - Day two
---
+++ website/siteConfig.js
@@ -0,0 +1,63 @@
+// See https://docusaurus.io/docs/site-config for all the possible
+// site configuration options.
+
+const siteConfig = {
+  title: 'IIIF 2 day Workshop', // Title for your website.
+  tagline: 'A website for testing',
+  url: 'https://your-docusaurus-test-site.com', // Your website URL
+  baseUrl: '/', // Base URL for your project */
+  // For github.io type URLs, you would set the url and baseUrl like:
+  //   url: 'https://facebook.github.io',
+  //   baseUrl: '/test-site/',
+
+  // Used for publishing and more
+  projectName: 'IIIF-2-Day-Workshop',
+  organizationName: 'mejackreed',
+  docsSideNavCollapsible: true,
+  // For top-level user or org sites, the organization is still the same.
+  // e.g., for the https://JoelMarcey.github.io site, it would be set like...
+  //   organizationName: 'JoelMarcey'
+
+  // For no header links in the top nav bar -> headerLinks: [],
+  headerLinks: [],
+
+  /* path to images for header/footer */
+  headerIcon: null,
+  footerIcon: null,
+  favicon: null,
+
+  /* Colors for website */
+  colors: {
+    primaryColor: '#3F51B5',
+    secondaryColor: '#F44336',
+  },
+
+  // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
+  copyright: `Copyright © ${new Date().getFullYear()} Jack Reed`,
+
+  highlight: {
+    // Highlight.js theme to use for syntax highlighting in code blocks.
+    theme: 'default',
+  },
+
+  // On page navigation for the current documentation page.
+  onPageNav: 'separate',
+  // No .html extensions for paths.
+  cleanUrl: true,
+
+  // Open Graph and Twitter card images.
+  ogImage: null,
+  twitterImage: null,
+
+  // Show documentation's last contributor's name.
+  // enableUpdateBy: true,
+
+  // Show documentation's last update time.
+  // enableUpdateTime: true,
+
+  // You may provide arbitrary config keys to be used as needed by your
+  // template. For example, if you need your repo's URL...
+  //   repoUrl: 'https://github.com/facebook/test-site',
+};
+
+module.exports = siteConfig;
