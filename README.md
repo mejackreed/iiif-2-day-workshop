b9c8ee2d56dcf065e7400b24336bff6cf3d1e61f
Jack Reed <phillipjreed@gmail.com>
2019-05-03 10:03:09 +0300

    Add readme and update siteconfig

---
+++ README.md
@@ -0,0 +1,5 @@
+# IIIF 2 Day Workshop
+
+Built using [docusaurus.io](https://docusaurus.io).
+
+See [website/README.md](./website/README.md)
--- website/siteConfig.js
+++ website/siteConfig.js
@@ -3,8 +3,8 @@
 
 const siteConfig = {
   title: 'IIIF 2 day Workshop', // Title for your website.
-  tagline: 'A website for testing',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
+  tagline: 'A IIIF workshop to start learning IIIF',
+  url: 'https://iiif-2-day-workshop.netlify.com', // Your website URL
   baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
@@ -19,7 +19,9 @@ const siteConfig = {
   //   organizationName: 'JoelMarcey'
 
   // For no header links in the top nav bar -> headerLinks: [],
-  headerLinks: [],
+  headerLinks: [
+    { href: "https://github.com/mejackreed/iiif-2-day-workshop", label: "Edit these docs" },
+  ],
 
   /* path to images for header/footer */
   headerIcon: null,
